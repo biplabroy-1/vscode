3f7796225bf521fa128f77ad21d7e23a5aadb9f9
Biplab Roy <biplabr119201@gmail.com>
2024-07-31 03:44:14 +0530

    init

---
+++ src/extension.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+		vscode.window.showInformationMessage('Hello, I am Biplab');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
